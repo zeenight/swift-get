c8c2cecb64468afb3e8d1fb5d706aa3a7482abab
zeenight <zillan.lambot@gmail.com>
2024-09-10 10:02:23 +0700

    up 4

--- main/models.py
+++ main/models.py
@@ -1,7 +1,5 @@
-from django.db import models
-
 # Create your models here.
-
+from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
