2b98edc6c3ce202bfcb9bacd6226a98bd5d0d412
zeenight <zillan.lambot@gmail.com>
2024-09-10 09:00:52 +0700

    tiny changes

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
+        'app_name' : 'swift get'
         'npm' : '2306275443',
         'name': 'Zillan Ahmad Ryandi',
         'class': 'PBP E'
