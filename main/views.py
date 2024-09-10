e2a78deb8afdcaad2622d156b0ec911aeaf99974
zeenight <zillan.lambot@gmail.com>
2024-09-10 10:13:45 +0700

    pws push

--- main/views.py
+++ main/views.py
@@ -4,9 +4,6 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'app_name' : 'swift get'
-        'npm' : '2306275443',
-        'name': 'Zillan Ahmad Ryandi',
-        'class': 'PBP E'
     }
 
     return render(request, "main.html", context)
