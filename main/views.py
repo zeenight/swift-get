1567990d7b16a9d7ad7c9cb82ec050768a38bb3b
zeenight <zillan.lambot@gmail.com>
2024-09-10 10:31:40 +0700

    fix html

--- main/views.py
+++ main/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'app_name' : 'swift get'
+        'app_name' : 'swift get',
+        'npm' : '2306275443',
+        'name': 'Zillan Ahmad Ryandi',
+        'class' : 'PBP F'
     }
 
     return render(request, "main.html", context)
