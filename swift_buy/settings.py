777f64fcd4be45c3d5ff5348edf20e1893602966
zeenight <zillan.lambot@gmail.com>
2024-10-01 12:13:10 +0700

    setting

--- swift_buy/settings.py
+++ swift_buy/settings.py
@@ -116,12 +116,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+...
+STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static' # merujuk ke /static root project pada mode development
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode production
+...
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
